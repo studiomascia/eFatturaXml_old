12ad90e7d8a7e1012df12fafa16431b3654b2d46
studiomascia <developer@studiomascia.it>
2019-09-04 11:01:20 +0200

    Revert "correzione di alcuni problemi"
    This reverts commit 511034138b2c119cd62dfb676cec660d26d9d91a.

--- src/main/java/it/studiomascia/gestionale/WebSecurityConfig.java
+++ src/main/java/it/studiomascia/gestionale/WebSecurityConfig.java
@@ -36,7 +36,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/resources/**").permitAll()
+                .antMatchers("/resources/**", "/registration").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
@@ -44,12 +44,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
             .logout()
-                .permitAll()
-				.and()
-            .welcome()
                 .permitAll();
- 
- }
+    }
 
     @Bean
     public AuthenticationManager customAuthenticationManager() throws Exception {
