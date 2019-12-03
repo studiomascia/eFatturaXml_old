07e2ac652a05e8422e99c31979c5e36e1e0443fd
studiomascia <ing.luigimascia@gmail.com>
2019-12-03 13:44:37 +0100

    export in excel e pdf delle tabelle fatture in e out

--- src/main/java/it/studiomascia/gestionale/controllers/ReportController.java
+++ src/main/java/it/studiomascia/gestionale/controllers/ReportController.java
@@ -5,17 +5,14 @@
  */
 package it.studiomascia.gestionale.controllers;
 
-import com.sun.rowset.internal.Row;
 import it.studiomascia.gestionale.models.User;
 import it.studiomascia.gestionale.repository.UserRepository;
-import it.studiomascia.gestionale.service.UtenteDetails;
-import java.awt.Font;
+import it.studiomascia.gestionale.service.XmlFatturaBaseService;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.lang.Thread.State;
 import java.util.List;
-import javafx.scene.control.Cell;
+import java.util.Map;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.CreationHelper;
@@ -39,7 +36,11 @@ public class ReportController
     @Autowired
     UserRepository userRepository;
     
-    @RequestMapping(value = "/Report", method = RequestMethod.GET)
+        
+    @Autowired
+    private XmlFatturaBaseService xmlFatturaBaseService;
+    
+    @RequestMapping(value = "/ReportTest", method = RequestMethod.GET)
     public ResponseEntity<InputStreamResource> excelReport() throws IOException {
         List<User> listaUtenti  = userRepository.findAll();
 
@@ -89,6 +90,83 @@ public class ReportController
                 rowTest.createCell(0).setCellValue("test_1");
                 rowTest.createCell(1).setCellValue("test_2");
                 rowTest.createCell(2).setCellValue("test_3");
+            
+            
+            workbook.write(out);
+            ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
+HttpHeaders headers = new HttpHeaders();
+    headers.add("Content-Disposition", "attachment; filename=customers.xlsx");
+
+    return ResponseEntity.ok().headers(headers).body(new InputStreamResource(in));
+        }
+
+    
+    
+    
+    
+    
+    
+    // return IOUtils.toByteArray(in);
+
+    
+}
+    
+    @RequestMapping(value = "/ReportInvoicesIn", method = RequestMethod.GET)
+    public ResponseEntity<InputStreamResource> excelReportInvoicesIn() throws IOException {
+       
+
+        List<String> COLUMNs = xmlFatturaBaseService.getHeaders();
+        List<Map<String,Object>> listaFatture  = xmlFatturaBaseService.getRows();
+
+        try (
+            Workbook workbook = new HSSFWorkbook(); 
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            ) 
+        {
+            CreationHelper createHelper = workbook.getCreationHelper();
+
+            Sheet sheet = workbook.createSheet("Fatture");
+
+            org.apache.poi.ss.usermodel.Font headerFont = workbook.createFont();
+            headerFont.setBold(true);
+            headerFont.setColor(IndexedColors.BLUE.getIndex());
+
+            CellStyle headerCellStyle = workbook.createCellStyle();
+            headerCellStyle.setFont(headerFont);
+
+            // Row for Header
+            org.apache.poi.ss.usermodel.Row headerRow = sheet.createRow(0);
+            int rowIdx = 1;
+            
+            
+            // Header
+            for (int col = 0; col < COLUMNs.size(); col++) {
+                org.apache.poi.ss.usermodel.Cell cell = headerRow.createCell(col);
+                cell.setCellValue(COLUMNs.get(col));
+                cell.setCellStyle(headerCellStyle);
+            }   
+            for (Map<String,Object> item : listaFatture) {
+                org.apache.poi.ss.usermodel.Row row = sheet.createRow(rowIdx++);
+                int indice=0;
+                 for (Map.Entry<String,Object> elemento : item.entrySet())  
+                 {
+                    if (elemento!=null)
+                        row.createCell(indice++).setCellValue(elemento.getValue().toString());
+                 }
+                
+                
+               
+   
+            }
+          
+
+            // CellStyle for Age
+            CellStyle ageCellStyle = workbook.createCellStyle();
+            ageCellStyle.setDataFormat(createHelper.createDataFormat().getFormat("#"));
+
+           
+
+            
             
             
             workbook.write(out);
