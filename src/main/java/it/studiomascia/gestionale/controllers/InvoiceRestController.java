f0ad5d2ccca11a33e4e91852bc46be4624640470
studiomascia <ing.luigimascia@gmail.com>
2019-12-04 18:28:03 +0100

    inserito la funzione AnagraficaUpdateList che aggiorna la lista dei fornitori dalle fatture elettroniche inserite

--- src/main/java/it/studiomascia/gestionale/controllers/InvoiceRestController.java
+++ src/main/java/it/studiomascia/gestionale/controllers/InvoiceRestController.java
@@ -6,24 +6,31 @@
 package it.studiomascia.gestionale.controllers;
 
 import it.studiomascia.gestionale.Utility;
+import it.studiomascia.gestionale.models.AnagraficaSocieta;
 import it.studiomascia.gestionale.models.FatturaVirtuale;
 import it.studiomascia.gestionale.models.XmlFatturaBase;
 import it.studiomascia.gestionale.models.XmlFatturaBasePredicate;
+import it.studiomascia.gestionale.repository.AnagraficaSocietaRepository;
 import it.studiomascia.gestionale.repository.XmlFatturaBaseRepository;
 import it.studiomascia.gestionale.service.XmlFatturaBaseService;
+import it.studiomascia.gestionale.xml.AnagraficaType;
 import it.studiomascia.gestionale.xml.FatturaElettronicaType;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.util.ArrayList;
+import static java.util.Collections.list;
 import java.util.Date;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.transform.stream.StreamSource;
+import org.apache.poi.ss.formula.functions.T;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
@@ -45,6 +52,8 @@ public class InvoiceRestController {
 private XmlFatturaBaseRepository xmlFatturaBaseRepository;
 @Autowired
 private XmlFatturaBaseService xmlFatturaBaseService;
+@Autowired
+private AnagraficaSocietaRepository  anagraficaSocietaRepository;
 
 @RequestMapping(path="/getFattureIn", method=RequestMethod.GET)
 public List<FatturaVirtuale> FatturePassiveList(){
@@ -102,7 +111,7 @@ public List<FatturaVirtuale> FatturePassiveList(){
   
 
 @RequestMapping(path="/ReportFattureIn", method=RequestMethod.GET)
-    public ResponseEntity<Resource> downloadReportFattureIn(String fileName) throws IOException {
+public ResponseEntity<Resource> downloadReportFattureIn(String fileName) throws IOException {
         String filename= "report.xlsx";
         
         
@@ -115,4 +124,83 @@ return ResponseEntity.ok()
         .body(resource);
 }
  
+@RequestMapping(path="/AnagraficaShortList", method=RequestMethod.GET)
+public List<AnagraficaSocieta> AnagraficaShortList(){
+        
+       
+        //FINE:: BLOCCO PER LA PAGINAZIONE
+        List<XmlFatturaBase> listaFatture = XmlFatturaBasePredicate.filterXmlFatturaBase(xmlFatturaBaseRepository.findAll(), XmlFatturaBasePredicate.isPassiva());
+        //List<AnagraficaSocieta> listaAnagrafica = new ArrayList<AnagraficaSocieta>();
+        ArrayList<AnagraficaSocieta> listaAnagrafica = new ArrayList<>();
+
+        String  strData = null;
+        byte[] byteArr;
+        try {
+            StringWriter sw = new StringWriter();
+            JAXBContext context = JAXBContext.newInstance(FatturaElettronicaType.class);
+            // Unmarshaller serve per convertire il file in un oggetto
+            Unmarshaller jaxbUnMarshaller = context.createUnmarshaller();
+            // Marshaller serve per convertire l'oggetto ottenuto dal file in una stringa xml
+            Marshaller jaxbMarshaller = context.createMarshaller();
+            jaxbMarshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+       
+            for (XmlFatturaBase xmlFattura:listaFatture) {
+                byteArr = xmlFattura.getXmlData().getBytes("UTF-8");
+                sw = new StringWriter();
+            
+                JAXBElement<FatturaElettronicaType> root =jaxbUnMarshaller.unmarshal(new StreamSource(new ByteArrayInputStream(byteArr)), FatturaElettronicaType.class);
+                FatturaElettronicaType item = root.getValue();
+                jaxbMarshaller.marshal(root, sw);
+
+                
+                String partitaIVA = item.getFatturaElettronicaHeader().getCedentePrestatore().getDatiAnagrafici().getIdFiscaleIVA().getIdCodice().toString();
+                String denominazione = item.getFatturaElettronicaHeader().getCedentePrestatore().getDatiAnagrafici().getAnagrafica().getDenominazione();
+                if (item.getFatturaElettronicaHeader().getCedentePrestatore().getDatiAnagrafici().getAnagrafica().getDenominazione()!=null)
+                {
+                    denominazione=item.getFatturaElettronicaHeader().getCedentePrestatore().getDatiAnagrafici().getAnagrafica().getDenominazione();
+                }else{
+                    AnagraficaType  anagrafica = item.getFatturaElettronicaHeader().getCedentePrestatore().getDatiAnagrafici().getAnagrafica();
+                    if (anagrafica.getTitolo()!=null)  denominazione+= anagrafica.getTitolo() +" ";
+                    if (anagrafica.getNome()!=null)  denominazione+= anagrafica.getNome() +" ";
+                    if (anagrafica.getCognome()!=null)  denominazione+= anagrafica.getCognome() +" ";
+                }
+                
+                
+                String indirizzo = item.getFatturaElettronicaHeader().getCedentePrestatore().getSede().getIndirizzo();
+                String civico = item.getFatturaElettronicaHeader().getCedentePrestatore().getSede().getNumeroCivico();
+                String cap = item.getFatturaElettronicaHeader().getCedentePrestatore().getSede().getCAP();
+                String comune = item.getFatturaElettronicaHeader().getCedentePrestatore().getSede().getComune();
+
+                AnagraficaSocieta riga = new AnagraficaSocieta();
+                riga.setPiva(partitaIVA );
+                riga.setDenominazione(denominazione );
+                riga.seFornitore(true);
+                riga.setIndirizzo(indirizzo +","+ civico+";"+ cap +";"+ comune);
+                listaAnagrafica.add(riga); 
+            }
+            Set<AnagraficaSocieta> newListaAnagrafica = new LinkedHashSet<>(listaAnagrafica);
+            listaAnagrafica.clear();
+            listaAnagrafica.addAll(newListaAnagrafica);
+
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } 
+        return listaAnagrafica;
+    }
+
+    @RequestMapping(path="/AnagraficaUpdateList", method=RequestMethod.GET)
+    public void AnagraficaUpdateList(){
+
+        List<AnagraficaSocieta> lista = AnagraficaShortList();
+        List<AnagraficaSocieta>listaDB = anagraficaSocietaRepository.findAll();
+        
+        for (AnagraficaSocieta var : lista) 
+        { 
+            if (anagraficaSocietaRepository.findByPiva(var.getPiva()) == null )
+            {
+                anagraficaSocietaRepository.save(var);}
+            } 
+    }
 }
---
+++ src/main/java/it/studiomascia/gestionale/repository/AnagraficaSocietaRepository.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.studiomascia.gestionale.repository;
+
+import it.studiomascia.gestionale.models.AnagraficaSocieta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Admin
+ */
+@Repository
+
+public interface AnagraficaSocietaRepository extends JpaRepository<AnagraficaSocieta, Integer> {
+     AnagraficaSocieta findByPiva(String piva);
+//     Boolean existByPiva(String piva);
+}
