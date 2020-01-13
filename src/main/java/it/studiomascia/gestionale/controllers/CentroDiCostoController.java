fddbe2c1e4ba51aaab6631fbaea4b7dba25e22aa
studiomascia <developer@studiomascia.it>
2020-01-13 15:39:05 +0100

    prova di gestione albero per Centri di Costo

---
+++ src/main/java/it/studiomascia/gestionale/controllers/CentroDiCostoController.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.studiomascia.gestionale.controllers;
+
+import it.studiomascia.gestionale.models.CentroDiCosto;
+import it.studiomascia.gestionale.repository.CentroDiCostoRepository;
+import java.util.List;
+import java.text.SimpleDateFormat;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+
+/**
+ *
+ * @author luigi
+ */
+@Controller
+public class CentroDiCostoController {
+    
+    private static final Logger logger = LoggerFactory.getLogger(CentroDiCostoController.class);
+
+    private SimpleDateFormat formattaData = new SimpleDateFormat("dd-MM-yyyy");
+
+    
+    @Autowired
+    private CentroDiCostoRepository centroDiCostoRepository;
+    
+    @GetMapping("/CentroDiCosto")
+    public String CentroDiCostoList(Model model){
+        
+        List<CentroDiCosto> lista =  centroDiCostoRepository.findAll();
+        model.addAttribute("lista_cdc", lista);
+        return "lista_cdc";
+    }
+    
+   
+  
+    
+
+}
